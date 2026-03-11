2f0e443452532c2e64e0eed5fbd5bf7e2c2b3067
remymoro <moro.remy879@gmail.com>
2026-03-11 17:04:56 +0100

    fix: import expect from @jest/globals in test-utils to fix shared lib build

--- libs/shared/src/test-utils.ts
+++ libs/shared/src/test-utils.ts
@@ -1,3 +1,4 @@
+import { expect } from '@jest/globals';
 import { DomainException } from './domain/exceptions/domain.exception';
 
 export function expectDomainCode(fn: () => void, code: string): void {
